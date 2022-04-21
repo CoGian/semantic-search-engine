e2f230e1efd597f1119e367700195a6cd14ec4ac
CoGian <kgiantsios@atypon.com>
2022-04-21 15:25:42 +0300

    add reranking and index

--- src/api.py
+++ src/api.py
@@ -1,5 +1,5 @@
 from typing import Optional
-
+from scipy.spatial import distance
 import yaml
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -18,8 +18,8 @@ model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
 
 class Query(BaseModel):
 	text: str
-	k: int = 5
-	distance_metric: Optional[str] = 'euclidian'
+	k: int = 10
+	distance_metric: Optional[str] = 'cosine'
 	present_paper: Optional[bool] = False
 
 
@@ -33,6 +33,36 @@ async def search(query: Query):
 	query_embedding = model.encode(sentences=[query.text])[0]
 	papers = ps_connector.get_k_results(query_embedding, query.k)
 
-	papers = {index: {'title': paper.title} for index, paper in enumerate(papers)}
+	reranked_papers = rerank(query, papers, query_embedding)
+	reranked_papers = {index: {
+		'title': paper.title,
+		'local_link': paper.local_link} for index, paper in enumerate(reranked_papers)}
+
+	return reranked_papers
+
+
+def rerank(query, papers, query_embedding):
+
+	if query.distance_metric == 'braycurtis':
+		distance_func = distance.braycurtis
+	elif query.distance_metric == 'canberra':
+		distance_func = distance.canberra
+	elif query.distance_metric == 'chebyshev':
+		distance_func = distance.chebyshev
+	elif query.distance_metric == 'cityblock':
+		distance_func = distance.cityblock
+	elif query.distance_metric == 'cosine':
+		distance_func = distance.cosine
+	elif query.distance_metric == 'euclidean':
+		distance_func = distance.euclidean
+	else:
+		# default dot product return papers with the same order
+		return papers
+
+	distances = [distance_func(query_embedding, paper.embedding) for paper in papers]
+	papers = [paper for _, paper in sorted(zip(distances, papers))]
 
 	return papers
+
+
+
--- src/postgres_connector.py
+++ src/postgres_connector.py
@@ -1,5 +1,5 @@
 from pgvector.sqlalchemy import Vector
-from sqlalchemy import create_engine, text, Column, Integer, String
+from sqlalchemy import create_engine, text, Column, String, Index
 from sqlalchemy.orm import declarative_base, Session
 
 Base = declarative_base()
@@ -27,6 +27,15 @@ class PostgresConnector:
 			conn.commit()
 
 		self.engine = engine
+		self.create_index()
+
+	def create_index(self, distance='vector_ip_ops'):
+		index = Index('my_index', Paper.embedding,
+					  postgresql_using='ivfflat',
+					  postgresql_with={'lists': 100},
+					  postgresql_ops={'factors': distance}
+					  )
+		index.create(self.engine, checkfirst=True)
 
 	def populate_db(self, papers):
 		Base.metadata.drop_all(self.engine)
@@ -39,7 +48,7 @@ class PostgresConnector:
 	def get_k_results(self, query_embedding, k=5):
 		session = Session(self.engine)
 		papers = session.query(Paper)\
-			.order_by(Paper.embedding.l2_distance(query_embedding))\
+			.order_by(Paper.embedding.max_inner_product(query_embedding))\
 			.limit(k)\
 			.all()
 		return papers
