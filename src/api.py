3a0a2c2d0fae1fc26254a18a7834713ed83a3d2a
CoGian <kgiantsios@atypon.com>
2022-04-23 20:39:20 +0300

    add reasoning

--- src/api.py
+++ src/api.py
@@ -7,6 +7,11 @@ from pydantic import BaseModel
 from sentence_transformers import SentenceTransformer
 import webbrowser
 from src.postgres_connector import PostgresConnector
+from nltk.corpus import stopwords
+from rapidfuzz import fuzz
+
+
+stop_words = set(stopwords.words('english'))
 
 app = FastAPI()
 
@@ -35,9 +40,12 @@ async def search(query: Query):
 	papers = ps_connector.get_k_results(query_embedding, query.k)
 
 	reranked_papers = rerank(query, papers, query_embedding)
+	explanations = get_explanation(query.text, reranked_papers)
 	reranked_papers = {index: {
 		'title': paper.title,
-		'local_link': paper.local_link} for index, paper in enumerate(reranked_papers)}
+		'local_link': paper.local_link,
+		'explanation': explanations[index]
+	} for index, paper in enumerate(reranked_papers)}
 
 	return reranked_papers
 
@@ -71,4 +79,39 @@ def rerank(query, papers, query_embedding):
 	return papers
 
 
+def get_explanation(query_text, reranked_papers):
 
+	filtered_query_words = [w.lower() for w in query_text.split() if not w.lower() in stop_words]
+
+	explanations = []
+	for paper in reranked_papers:
+		paper_text = paper.title + " " + paper.abstract
+		paper_words = paper_text.split()
+		query_word_index_list = [[] for _ in filtered_query_words]
+		for i, query_word in enumerate(filtered_query_words):
+			for j, paper_word in enumerate(paper_words):
+				if fuzz.ratio(query_word, paper_word, score_cutoff=90.0):
+					query_word_index_list[i].append(j)
+
+		flat_query_word_index_list = sorted(set([index for sublist in query_word_index_list for index in sublist]))
+		explanation = []
+		for index, paper_word_index in enumerate(flat_query_word_index_list):
+			if index > 0 and paper_word_index - flat_query_word_index_list[index - 1] > 5:
+				explanation.append(paper_word_index)
+			elif index == 0:
+				explanation.append(paper_word_index)
+
+		explanation_sents = []
+		for index in explanation:
+			lo = index - 5
+			hi = index + 5
+			if lo < 0:
+				lo = 0
+			if hi > len(paper_words):
+				hi = len(paper_words)
+
+			explanation_sents.append(" ".join(paper_words[lo:hi]))
+
+		explanations.append(explanation_sents)
+
+	return explanations
